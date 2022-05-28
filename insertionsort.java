b7a68e7cb135d916e2c0b3768083fe7d20e1fa02
Rajat99-glitch <62611665+Rajat99-glitch@users.noreply.github.com>
2022-05-28 09:07:49 +0530

    Create insertionsort.java

---
+++ insertionsort.java
@@ -0,0 +1,45 @@
+public class MyClass {
+    public static void swap(int arr[],int i,int j)
+    {
+        if(arr[i]>arr[j])
+        {
+            System.out.println("Swaping " +arr[i]+ " to " +arr[j]);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    
+    public static boolean isGreater(int arr, int i, int j)
+    {
+        if(arr[i] > arr[j])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public static void selectionSort(int arr[])
+    {
+        int n = arr.length;
+        for(int i =0; i<n-1; i++)
+        {
+            for(int j =i+1; j<n;j++)
+            {
+               swap(arr, i, j);
+            }
+        }
+        for(int i =0;i<n;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String args[]) {
+        int arr[]={5,9,8,2,1};
+      selectionSort(arr);
+      //System.out.println("Sum of x+y = " + selectionSort(arr));
+    }
+}
