4024d8957c09ca56bd1490cdcd92c9fee8f0b9fd
Rajat99-glitch <62611665+Rajat99-glitch@users.noreply.github.com>
2022-05-28 09:24:52 +0530

    Update insertionsort.java

--- insertionsort.java
+++ insertionsort.java
@@ -1,57 +1,46 @@
-public class MyClass {
+public class Findswapnumbers {
     public static void swap(int arr[],int i,int j)
     {
-        if(arr[i]>arr[j])
-        {
             System.out.println("Swaping " +arr[i]+ " to " +arr[j]);
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
-        }
     }
-    
-    public static boolean isGreater(int arr, int i, int j)
+    public static boolean isGreater(int arr[], int i, int j)
     {
         if(arr[i] > arr[j])
-        {
             return true;
-        }
         else
-        {
             return false;
-        }
     }
-    
-    public static boolean isSmaller(int arr, int i, int j)
+    public static boolean isSmaller(int arr[], int i, int j)
     {
         if(arr[i] < arr[j])
-        {
             return true;
-        }
         else
-        {
             return false;
-        }
     }
-    
-    public static void selectionSort(int arr[])
+    public static void insertionSort(int arr[])
     {
         int n = arr.length;
-        for(int i =0; i<n-1; i++)
+        for(int i = 1; i < n; i++)
         {
-            for(int j =i+1; j<n;j++)
+            for(int j = i-1; j>=0; j--)
             {
-               swap(arr, i, j);
+                if(isGreater(arr, j, j+1))
+                {
+                    swap(arr, j+1 , j);
+                }
             }
-        }
-        for(int i =0;i<n;i++)
-        {
-            System.out.println(arr[i]);
         }
     }
     public static void main(String args[]) {
         int arr[]={5,9,8,2,1};
-      selectionSort(arr);
+      insertionSort(arr);
+      for(int i =0; i<arr.length; i++)
+      {
+         System.out.println(arr[i]);
+      }
       //System.out.println("Sum of x+y = " + selectionSort(arr));
     }
 }
